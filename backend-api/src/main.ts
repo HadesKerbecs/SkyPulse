69ff3532161cf476e4fe1e6d8221ffd21aa73603
Eduardo Tristão Melo <eduardotm22@gmail.com>
2026-02-19 09:02:23 -0300

    fix render port

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('Backend rodando na porta 3000…');
 }
 bootstrap();
